941c457f7d2230809b8bd3175c4bc7ee91515929
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2026-03-10 14:31:31 +0100

    export GLOB_IGNORE_PATTERNS

--- out/lib.d.ts
+++ out/lib.d.ts
@@ -3,4 +3,5 @@ export { talonFormatter } from "./talon/talonFormatter.js";
 export { talonListFormatter } from "./talon/talonListFormatter.js";
 export { treeSitterFormatter } from "./treeSitterFormatter.js";
 export type { Options, SyntaxNode } from "./types.js";
+export { GLOB_IGNORE_PATTERNS } from "./util/constants.js";
 export { getOptionsFromConfig } from "./util/getOptionsFromConfig.js";
