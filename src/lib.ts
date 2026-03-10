90b267fcb0e66f5ea1b6aafaaa33779433bc69c8
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2026-03-10 05:04:07 +0100

    Export getOptionsFromConfig and Options from lib.ts

--- src/lib.ts
+++ src/lib.ts
@@ -1,4 +1,6 @@
+export { snippetFormatter } from "./snippet/snippetFormatter.js";
 export { talonFormatter } from "./talon/talonFormatter.js";
 export { talonListFormatter } from "./talon/talonListFormatter.js";
 export { treeSitterFormatter } from "./treeSitterFormatter.js";
-export { snippetFormatter } from "./snippet/snippetFormatter.js";
+export type { Options } from "./types.js";
+export { getOptionsFromConfig } from "./util/getOptionsFromConfig.js";
